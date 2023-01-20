bd1ff4467322e47f1c983f96ff0c881c4d8a7a8e
Lucas Mariano dos Santos <90803753+lmariano1308@users.noreply.github.com>
2023-01-20 00:48:13 -0300

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Container } from "./components/background/style";
+
+function App() {
+  return (
+    <Container>
+      <h1>React TypeScript</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/background/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+interface IBackgroundProps {
+  children: React.ReactNode;
+}
+
+function Background({ children }: IBackgroundProps) {
+  return <Container>{children}</Container>;
+}
+
+export default Background;
